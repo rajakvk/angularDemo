21b91ecf421a4be5ea22df8a205861b1325f5a14
rajakvk <rajakvk@gmail.com>
2015-08-09 13:19:00 +0530

    Fixing concatenate issue

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,9 @@
 function calculate() {
-    var basic = document.querySelector('#basic').value;
-    var hra = document.querySelector('#hra').value;
-    var salary = basic + hra;
+
+    var basic   = document.querySelector('#basic').value,
+        hra     = document.querySelector('#hra').value,
+        salary  = Number.parseInt(basic) + Number.parseInt(hra);
+
     document.querySelector('#salary span').innerHTML = salary;
+
 }
