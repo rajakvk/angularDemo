b94b1f06b63798e988586602f5d6aed1f35567d2
rajakvk <rajakvk@gmail.com>
2015-08-10 00:03:26 +0530

    mimick of app-click directive

--- assets/scripts/scopeDemo.js
+++ assets/scripts/scopeDemo.js
@@ -20,25 +20,11 @@ SalaryCalculator.prototype.calculate = function(){
 
 scope.calculator = new SalaryCalculator();
 
-// function basicChange(){
-//     var that = this;
+// function calculate(){
 //     scope.$apply(function(){
-//         scope.calculator.basic = that.value;
+//         scope.calculator.calculate();
 //     });
 // }
-
-// function hraChange(){
-//     var that = this;
-//     scope.$apply(function(){
-//         scope.calculator.hra = that.value;
-//     });
-// }
-
-function calculate(){
-    scope.$apply(function(){
-        scope.calculator.calculate();
-    });
-}
 
 function bindDirective() {
     var ele = document.querySelectorAll('input[app-model]');
@@ -70,25 +56,24 @@ function onewayDirective() {
         });
     }
 }
-// scope.$watch("calculator.basic", function(newValue, oldValue){
-//     document.querySelector('#basic').innerHTML = newValue;
-//     var salary = newValue.toInt() + scope.calculator.hra.toInt();
-//     document.querySelector('#salary span').innerHTML = salary;
-// });
 
-// scope.$watch("calculator.hra", function(newValue, oldValue){
-//     document.querySelector('#hra').innerHTML = newValue;
-//     var salary = scope.calculator.basic.toInt() + newValue.toInt();
-//     document.querySelector('#salary span').innerHTML = salary;
-// });
-
-// scope.$watch("calculator.salary", function(newValue, oldValue){
-//     document.querySelector('#salary span').innerHTML = newValue;
-// });
-
-// document.querySelector('#basic').onchange = basicChange;
-// document.querySelector('#hra').onchange = hraChange;
-document.querySelector('#calculate').onclick = calculate;
+function appClickDirective() {
+    var ele = document.querySelectorAll('[app-click]');
+    for(var i=0;i<ele.length;i++) {
+        var that = ele[i];
+        ele[i].onclick = function(){
+            var model = that.attributes['app-click'].value;
+            scope.$apply(function(){
+                scope.$eval(model);
+            })
+        }
+    }
+}
+scope.$watch('calculator.salary', function(newValue, oldValue){
+    document.querySelector('#salary').innerHTML = newValue;
+});
+// document.querySelector('#calculate').onclick = calculate;
 
 bindDirective();
 onewayDirective();
+appClickDirective();
