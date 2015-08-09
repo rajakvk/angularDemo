4f824c595137e5071bfff68784878367b753a83e
rajakvk <rajakvk@gmail.com>
2015-08-09 20:09:40 +0530

    subscribe to $watch

--- assets/scripts/scopeDemo.js
+++ assets/scripts/scopeDemo.js
@@ -12,3 +12,16 @@ function SalaryCalculator(){
 
 scope.calculator = new SalaryCalculator();
 
+scope.$watch("calculator.basic", function(newValue, oldValue){
+    var salary = parseInt(newValue) + parseInt(scope.calculator.hra);
+    document.querySelector('#salary span').innerHTML = salary;
+});
+
+scope.$watch("calculator.hra", function(newValue, oldValue){
+    var salary = parseInt(newValue) + parseInt(scope.calculator.basic);
+    document.querySelector('#salary span').innerHTML = salary;
+});
+
+scope.$watch("calculator.salary", function(newValue, oldValue){
+    document.querySelector('#salary span').innerHTML = newValue;
+});
