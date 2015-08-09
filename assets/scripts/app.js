0aa55288160cc0ed9e309f21fd68a1cc9cd8cbdd
rajakvk <rajakvk@gmail.com>
2015-08-09 13:35:40 +0530

    Adding toInt method to String with documentation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,32 @@
+/**
+ * adding toInt method to String object
+ * author rajakvk <rajakvk at gmail dot com>
+ */
+if(!String.toInt) {
+  /**
+   * Returns integer value if a string value is passed.
+   * @name toInt
+   * @methodOf String
+   * @param {*} none 
+   * @returns {Number}
+   * @example 
+   * $('#hra').val().toInt();
+   * typeof $('#hra').val();  // string
+   * typeof $('#hra').val().toInt();  // number
+   */
+    String.prototype.toInt = function(){
+
+        return this.length ? Number.parseInt(this, 10) : this;
+
+    }
+
+}
+
 function calculate() {
 
-    var basic   = document.querySelector('#basic').value,
-        hra     = document.querySelector('#hra').value,
-        salary  = Number.parseInt(basic) + Number.parseInt(hra);
+    var basic   = document.querySelector('#basic').value.toInt(),
+        hra     = document.querySelector('#hra').value.toInt(),
+        salary  = basic + hra;
 
     document.querySelector('#salary span').innerHTML = salary;
 
